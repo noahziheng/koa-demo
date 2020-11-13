0ea0f8fa957efec6174a260ad0a0f6cc4bad6e81
ziheng.gzh <ziheng.gzh@alibaba-inc.com>
2020-11-13 15:59:50 +0800

    feat: add Data

---
+++ data.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const dataFilePath = './data.json';
+
+module.exports = {
+  getList: () => {
+    const jsonStr = fs.readFileSync(dataFilePath);
+    return JSON.parse(jsonStr);
+  },
+  getItem: (i) => {
+    const jsonStr = fs.readFileSync(dataFilePath);
+    const list = JSON.parse(jsonStr);
+    return list[i];
+  },
+  createItem: (item) => {
+    const jsonStr = fs.readFileSync(dataFilePath);
+    const list = JSON.parse(jsonStr);
+    list.push(item);
+    return fs.writeFileSync(JSON.stringify(list));
+  },
+  updateItem: (i, item) => {
+    const jsonStr = fs.readFileSync(dataFilePath);
+    const list = JSON.parse(jsonStr);
+    list[i] = item;
+    return fs.writeFileSync(JSON.stringify(list));
+  },
+  removeItem: (i) => {
+    const jsonStr = fs.readFileSync(dataFilePath);
+    const list = JSON.parse(jsonStr);
+    list.splice(i, 1);
+    return fs.writeFileSync(JSON.stringify(list));
+  }
+};
--- index.js
+++ index.js
@@ -3,10 +3,41 @@ const serve = require('koa-static');
 
 const app = new Koa();
 
+const route = (method, url, handlerFn) => {
+  return async (ctx, next) => {
+    if (ctx.request.method === method && ctx.request.url === url) {
+      await handlerFn(ctx, next);
+    } else {
+      await next();
+    }
+  };
+};
+
 app.use(serve('./public'));
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+app.use(route('GET', '/api/todos', (ctx) => {
+  // 获取 Todo 项列表
+  ctx.body = 'GET /api/todos';
+}));
+
+app.use(route('GET', '/api/todo', (ctx) => {
+  // 获取 Todo 项
+  ctx.body = 'GET /api/todo';
+}));
+
+app.use(route('POST', '/api/todo', (ctx) => {
+  // 新增 Todo 项
+  ctx.body = 'POST /api/todo';
+}));
+
+app.use(route('PATCH', '/api/todo', (ctx) => {
+  // 更新 Todo 项
+  ctx.body = 'PATCH /api/todo';
+}));
+
+app.use(route('DELETE', '/api/todo', (ctx) => {
+  // 删除 Todo 项
+  ctx.body = 'DELETE /api/todo';
+}));
 
 app.listen(3000);
